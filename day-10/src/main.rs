b548aaf803e5a6998794663a7e83dd9e96c831d7
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-01-05 16:42:45 +0100

    Day 10 - Part Two

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
 		.collect();
 
 	part1(&lines);
+	part2(&lines);
 }
 
 fn char_to_int(c: char) -> i16 {
@@ -17,6 +18,16 @@ fn char_to_int(c: char) -> i16 {
 		']' => -57,
 		'}' => -1197,
 		'>' => -25137,
+		_ => 0,
+	}
+}
+
+fn int_to_int(i: i16) -> i16 {
+	match i {
+		3 => 1,
+		57 => 2,
+		1197 => 3,
+		25137 => 4,
 		_ => 0,
 	}
 }
@@ -40,4 +51,41 @@ fn part1(lines: &Vec<String>) {
 	}
 
 	println!("Part One: {}", illegal);
+}
+
+fn part2(lines: &Vec<String>) {
+	let mut illegals: Vec<i64> = Vec::new();
+
+	for line in lines {
+		let mut q: Vec<i16> = Vec::new();
+		let mut has_illegal = false;
+
+		for c in line.chars() {
+			let cv = char_to_int(c);
+			if cv > 0 {
+				q.push(cv);
+			} else if cv < 0 {
+				let top = q.pop().unwrap();
+				if top != -cv {
+					q.push(top);
+					has_illegal = true;
+					break;
+				}
+			}
+		}
+
+		if !has_illegal && q.len() > 0 {
+			let mut i: i64 = 0;
+			q.reverse();
+			for n in q {
+				i *= 5;
+				i += int_to_int(n) as i64;
+			}
+			illegals.push(i);
+		}
+	}
+
+	illegals.sort();
+
+	println!("Part Two: {:?}", illegals[illegals.len() / 2]);
 }
